e1a9f68a7407351d10f9c65cb18ead756b4a97b3
Oleh Onufryk <olehonufryk@gmail.com>
2021-03-29 22:40:04 +0300

    configured sonarcloud

--- src/main/java/com/project/kuhniia/dto/mapper/UserMapper.java
+++ src/main/java/com/project/kuhniia/dto/mapper/UserMapper.java
@@ -3,12 +3,9 @@ package com.project.kuhniia.dto.mapper;
 import com.project.kuhniia.dto.UserDto;
 import com.project.kuhniia.entity.User;
 import org.mapstruct.Mapper;
-import org.mapstruct.factory.Mappers;
 
 @Mapper
 public interface UserMapper {
-
-//    UserDtoMapper INSTANCE = Mappers.getMapper(UserDtoMapper.class);
 
     UserDto userToUserDto(User user);
 
